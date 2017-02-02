5db65c1cc5ce1859b901c4fb70a0f3626eef11ac
shenz <shenz@SZ-SURFACEPRO3>
2017-02-02 22:08:34 +0800

    增加XDocument的引用

---
+++ SourceCode/Framework/TestProjects/MCS.Library.Core.Test/Extensions/XElementExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using MCS.Library.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+
+namespace MCS.Library.Core.Test.Extensions
+{
+    public class XElementExtensionsTest
+    {
+        [Fact]
+        public void AttributeValueTest()
+        {
+            XElement root = PrepareRootXElement();
+
+            Assert.Equal(100, root.Attribute("shelfID", 0));
+        }
+
+        [Fact]
+        public void AttributeDefaultValueTest()
+        {
+            XElement root = PrepareRootXElement();
+
+            Assert.Equal(1024, root.Attribute("storeID", 1024));
+        }
+
+        private static XElement PrepareRootXElement()
+        {
+            return XElement.Parse("<Books shelfID='100'><Book id='01'><Name>一地鸡毛</Name></Book></Books>");
+        }
+    }
+}
