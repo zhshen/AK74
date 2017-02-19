3533d4bc5e3dbbd084a993ce9d2c5c6e3268a7f0
shenz <shenz@SZ-SURFACEPRO3>
2017-02-20 00:27:29 +0800

    调整目录结构

---
+++ SourceCode/Framework/Src/MCS.Library.Core/Caching/Dependencies/UdpCacheNotifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace MCS.Library.Core.Caching.Dependencies
+{
+    public class UdpCacheNotifier
+    {
+
+    }
+}
